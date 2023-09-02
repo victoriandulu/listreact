4264da8845ccfb5a4415c10555b95c8a08a5d6a1
unknown <judyvicm1@gmail.com>
2023-09-02 13:31:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const  [list,setList] = useState([]);
+  const  [input,setInput] = useState("");
+  
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+    setList([...list,newTodo]) ;
+  setInput('');
+ };
+const deleteTodo = (id) =>{
+  const newList = list.filter((todo) => todo.id !== id);
+
+  setList(newList);
 }
 
-export default App;
+  return (
+    <div className="form">
+    <div className='head'>
+      <h1>
+        My Todo List
+      </h1>
+      </div>
+      <div className='input'>
+  <input
+  
+    type='text'
+    value={input}
+    placeholder='Enter Something'
+    onChange={(e) => setInput(e.target.value)}
+  />
+  <button onClick={() => addTodo(input)}>Add</button>
+</div>
+<ul>
+  {list.map((todo) =>(
+    <li key={todo.id}> 
+    {todo.todo}
+  
+    <button onClick={() =>deleteTodo(todo.id)}>delete</button>
+    
+    </li>))}
+</ul>
+    </div>
+    
+  )
+}
+
+export default App
+ 
